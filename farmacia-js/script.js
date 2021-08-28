16426bcdc9737049708f51ad2ba90de982c39571
Yara Viana <yaralviana@gmail.com>
2021-08-28 17:58:35 -0300

    segue exercício da farmácia JS

---
+++ farmacia-js/script.js
@@ -0,0 +1,11 @@
+var produto1 = prompt('Qual produto você deseja?');
+var produto2 = prompt('Mais alguma coisa?');
+
+if(produto1 == produto2)
+{
+    alert('Parabéns. Você recebeu um desconto de R$5')
+}
+else
+{
+    alert('Agradecemos a sua compra :)');
+}
